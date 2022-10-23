ccf094ca29b6d93ccca66e2a047bbbb40bb2f23f
nhuhoang0701 <nhuhoang.fd71@gmail.com>
2022-10-23 22:54:19 +0200

    forward stepwise regression

--- gen_models_and_functions_D2_P1_G.R
+++ gen_models_and_functions_D2_P1_G.R
@@ -1,61 +1,102 @@
-rm(list=ls())
+rm(list = ls())
 library(caret)
 library(klaR)
+library(gridExtra)
+library(tidyverse)
+library(leaps)
+library(MASS)
 
 set.seed(1729) # the Hardy CRamanujan number
 
-# 1. Apprentissage des modèles
+# I. Apprentissage des modèles
 
-######### 1.A. REGRESSION : a completer
-model.reg <- {
-  
+######### 1.A. REGRESSION: Forward stepwise Regression
+# chargement des données
+reg.data <- read.table(file = "data/TPN1_a22_reg_app.txt", header = TRUE)
+rows <- nrow(reg.data)
+cols <- ncol(reg.data) - 1
+train_size <- 2 / 3
+nb_train <- round(train_size * rows)
+nb_test <- rows - nb_train
+
+# Separation des données en test et train
+train <- sample(1:rows, nb_train)
+reg.train <- reg.data[train, ]
+reg.test <- reg.data[-train, ]
+
+# apprentissage du modèle
+MSE <- function(y_test, y_predict) {
+    mean((y_test - y_predict)^2)
 }
 
-######### 1.B. CLASSIFICATION : Apprentissage d'un modèle de Regularized Discriminant Analysis 
+train.control <- trainControl(method = "cv", number = 5)
+model.reg <- train(y ~ .,
+    data = reg.train,
+    method = "leapForward",
+    tuneGrid = data.frame(nvmax = 1:100),
+    trControl = train.control
+)
+
+# Best set of predictors
+fwd.coefs <- coef(model.reg$finalModel, model.reg$bestTune$nvmax)
+fwd.pred.names <- names(fwd.coefs)[1:model.reg$bestTune$nvmax + 1]
+
+# Fit linear regression, with the above predictors
+fwd.model.linreg <- lm(paste("y", "~", paste(fwd.pred.names, collapse = " + ")),
+    data = reg.train
+)
+
+
+######### 1.B. CLASSIFICATION : Apprentissage d'un modèle de Regularized Discriminant Analysis
 
 # chargement des données
-setwd("/Users/cecileasselin/Desktop/SY19/TD4/")
-classif <- read.table(file = "data/TPN1_a22_clas_app.txt") 
+# setwd("/Users/cecileasselin/Desktop/SY19/TD4/")
+classif <- read.table(file = "data/TPN1_a22_clas_app.txt")
 classif$y<-as.factor(classif$y)
 
 # Separation des données en test et train
 n <- nrow(classif)
-nb.train <- round(2*n/3) 
+nb.train <- round(2 * n / 3)
 nb.test <- n - nb.train
-train <- sample(1:n, nb.train) 
-classif.train <- classif[train,] 
-classif.test <- classif[-train,]
+train <- sample(1:n, nb.train)
+classif.train <- classif[train, ]
+classif.test <- classif[-train, ]
 
 # apprentissage du modèle : on cherche les meilleurs paramètres lambda et gamma pour la RDA
-cv_5_grid = trainControl(method = "cv", number = 5)
-fit_rda_grid = train(y ~ ., data=classif.train, method = "rda", trControl = cv_5_grid)
+cv_5_grid <- trainControl(method = "cv", number = 5)
+fit_rda_grid <- train(y ~ ., data = classif.train, method = "rda", trControl = cv_5_grid)
 fit_rda_grid
 plot(fit_rda_grid)
 # The final values used for the model were gamma = 0 and lambda = 0.5
 
 # création du modèle avec les paramètres appris
-model.cls <- rda(y~., data=classif.train, gamma=0, lambda=0.5) 
+model.cls <- rda(y ~ ., data = classif.train, gamma = 0, lambda = 0.5)
 
 
 
-# 2. Création des fonctions de prédiction
+# II. Création des fonctions de prédiction
 
 
-# 2.A. REGRESSION : à completer
+# 2.A. REGRESSION
 prediction_reg <- function(dataset) {
-  # Ne pas oublier de charger **à l'intérieur de la fonction** les # bibliothèques utilisées.
-  library(...)
-  # Attention à ce que retourne un modèle en prédiction. Par exemple, # la lda retourne une liste nommée. On sélectionne alors les classes.
-  predict(clas, test_set)$class
+	library(gridExtra)
+	library(tidyverse)
+	library(leaps)
+	library(MASS)
+    pred.fwd <- predict(fwd.model.linreg, newdata = dataset)
+    pred.fwd
 }
+pred.fwd <- prediction_reg(reg.test)
+MSE(reg.test$y, pred.fwd)
 
-# 2.B. CLASSIFICATION 
-prediction_cls <- function(dataset) { 
-  # load lib for RDA
-  library(klaR)
-  
-  pred.rda<-predict(model.cls, dataset)$class
-  pred.rda
+
+# 2.B. CLASSIFICATION
+prediction_cls <- function(dataset) {
+    # load lib for RDA
+    library(klaR)
+
+    pred.rda <- predict(model.cls, dataset)$class
+    pred.rda
 }
 
 # Affichage du taux d'erreur qu'on aurait obtenu avec classif.test
@@ -68,10 +109,9 @@ mean(classif.test$y != pred.rda)
 # Sauvegarder également les objets utilisés dans ces fonctions (`model.reg` et `model.cls` dans l'exemple) !
 
 save(
-  "model.reg",
-  "model.cls",
-  "prediction_reg",
-  "prediction_cls",
-  file = "env.Rdata"
+    "model.reg",
+    "model.cls",
+    "prediction_reg",
+    "prediction_cls",
+    file = "env.Rdata"
 )
-
