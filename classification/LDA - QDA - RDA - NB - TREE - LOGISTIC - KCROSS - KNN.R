b2c9550ee667ccec92846c1585f7140408701192
Cecile Asselin <cecile.asselin@etu.utc.fr>
2022-10-23 22:49:59 +0200

    multiclass ROC

--- classification/LDA - QDA - RDA - NB - TREE - LOGISTIC - KCROSS - KNN.R
+++ classification/LDA - QDA - RDA - NB - TREE - LOGISTIC - KCROSS - KNN.R
@@ -1,16 +1,16 @@
-#setwd("/Users/cecileasselin/Desktop/SY19/TD4/")
+# setwd("/Users/cecileasselin/Desktop/SY19/TD4/")
 rm(list=ls())
 
 ############# CLASSIFICATION ####################
 
-#classif <- read.table(file='./Data/TPN1_a22_clas_app.txt', header = TRUE)
+# classif <- read.table(file='./Data/TPN1_a22_clas_app.txt', header = TRUE)
 classif <- read.table(file='TPN1_a22_clas_app.txt', header = TRUE)
 head(classif) 
-# données quantitatives de X1 à X45, données qualitatives X46 à X50, y est la donnée de classe à prédire (3 classes différentes)
+# donn??es quantitatives de X1 ?? X45, donn??es qualitatives X46 ?? X50, y est la donn??e de classe ?? pr??dire (3 classes diff??rentes)
 summary(classif)
 
 
-###### 0. Separation des données en test et train
+###### 0. Separation des donn??es en test et train
 n <- nrow(classif)
 nb.train <- round(2*n/3) 
 nb.test <- n - nb.train
@@ -20,7 +20,7 @@ classif$y<-as.factor(classif$y)
 classif$y
 
 # seed
-set.seed(1729) # the Hardy–Ramanujan number
+set.seed(1729) # the Hardy?CRamanujan number
 
 # Training/Testing data
 train <- sample(1:n, nb.train) 
@@ -35,10 +35,10 @@ lda.classif <- lda(y~., data=classif.train)
 pred.classif.lda <- predict(lda.classif, newdata=classif.test)
 summary(pred.classif.lda)
 
-# calcul du taux d'erreur de ma prédiction : on est à 0.491018
+# calcul du taux d'erreur de ma pr??diction : on est ?? 0.491018
 err.lda <- mean(pred.classif.lda$class!=classif.test$y)
 err.lda
-# autre manière de calculer erreur empirique  
+# autre mani??re de calculer erreur empirique  
 # matrix.conf.lda <- table(classif.test$y, pred.classif.lda$class) 
 # matrix.conf.lda
 # err.lda <- 1-sum(diag(matrix.conf.lda))/nb.test 
@@ -46,7 +46,7 @@ err.lda
 sprintf("Taux d'erreur de LDA : %f",err.lda)
 
 ####### 2. QDA
-# on n'a peut être pas assez de données pour faire une ADQ correcte (si besoin donner plus de données à l'ensemble d'apprentissage, et faire cross-validation)
+# on n'a peut ??tre pas assez de donn??es pour faire une ADQ correcte (si besoin donner plus de donn??es ?? l'ensemble d'apprentissage, et faire cross-validation)
 fit.qda <- qda(y~.,data=classif.train)
 pred.qda <- predict(fit.qda,newdata=classif.test) 
 err.qda <- mean(classif.test$y != pred.qda$class) #0.3473054
@@ -63,14 +63,14 @@ library(naivebayes)
 fit.naive <- naive_bayes(y~.,data=classif.train) 
 pred.naive <- predict(fit.naive,newdata=classif.test)  ###Warning message:
 #predict.naive_bayes(): more features in the newdata are provided as there are probability tables in the object. Calculation is performed based on features to be found in the tables. 
-err.naive <- mean(pred.naive != classif.test$y) # on est à 0.3532934，meilleurs résultats qu'avec ADL
-# Autre manière de calculer l'erreur
+err.naive <- mean(pred.naive != classif.test$y) # on est ?? 0.3532934??meilleurs r??sultats qu'avec ADL
+# Autre mani??re de calculer l'erreur
 # conf.naive <- table(classif.test$y,pred.naive) 
 # conf.naive
 # err.naive <- 1-sum(diag(conf.naive))/nb.test 
 # err.naive
 
-# ne pas oublier de vérifier l'hypothèse forte d’indépendance entre les prédicteurs faite ac NB (i.e. ???????? ??? ????????)
+# ne pas oublier de v??rifier l'hypoth??se forte d??ind??pendance entre les pr??dicteurs faite ac NB (i.e. ???????? ??? ????????)
 # quel test faire ? chisq test ou pearson
 library(vcd)
 mytable<-xtabs(~X1+X2,data=classif)
@@ -82,10 +82,10 @@ library(rpart)
 library(rpart.plot)
 tree <- rpart(y~., data = classif.train, method="class",subset=train, parms = list(split = 'gini'))
 rpart.plot(tree, box.palette="RdBu", shadow.col="gray",fallen.leaves=FALSE)
-plotcp(fit)
+# plotcp(fit)
 
 ## Cross-validation error (figure)
-## reste à faire
+## reste ?? faire
 
 # Pruning
 pruned_tree<-prune(tree,cp=0.026)
@@ -95,7 +95,7 @@ rpart.plot(pruned_tree, box.palette="RdBu", shadow.col="gray", fallen.leaves=FAL
 pred.tree=predict(pruned_tree,newdata=classif.test,type='class') # yhat
 matrix.pred.tree<-table(classif.test$y,pred.tree)
 err.tree<-1-mean(classif.test$y==pred.tree) # OU err<-1-sum(diag(matrix.pred.tree))/nb.test
-err.tree # on est à 0.5149701
+err.tree # on est ?? 0.5149701
 
 ###### 6. multinomial logistic regression
 library(nnet)
@@ -108,7 +108,7 @@ z<-summary(multi.logistreg)$coefficients/summary(multi.logistreg)$standard.error
 pvalue<- (1 - pnorm(abs(z), 0, 1)) * 2
 pvalue
 
-#相对危险度（相对危险风险比，odds，与OR等价）relative risk ratio
+#????危?斩龋?????危?辗??毡龋?odds????OR?燃郏?relative risk ratio
 exp(coef(multi.logistreg))
 
 head(pp <- fitted(multi.logistreg))
@@ -120,7 +120,7 @@ err.logistreg <- mean(pred.multi.logistreg!=classif.test$y)
 # ou
 #matrix.multi.logistreg<-table(classif.test$y,pred.multi.logistreg)
 #err.logistreg<- 1-sum(diag(matrix.multi.logistreg))/nb.test
-err.logistreg #on est à 0.4850299
+err.logistreg #on est ?? 0.4850299
 
 
 ###### 7. Comparaison LDA / QDA / RDA / NB / TREE / LOGISTIC
@@ -131,34 +131,47 @@ sprintf("Taux d'erreur de Naive Bayes : %f",err.naive)
 sprintf("Taux d'erreur de classification/decision trees : %f",err.tree)
 sprintf("Taux d'erreur de multinomial logistic regression : %f",err.logistreg)
 
-library(pROC)
-roc.lda <- roc(classif.test$y,as.vector(pred.classif.lda$posterior[,1])) 
-roc.qda <- roc(classif.test$y,as.vector(pred.qda$posterior[,1])) # OU roc.qda <- roc(classif.test$y,as.vector(pred.qda[,1]))
-roc.rda <- roc(classif.test$y,as.vector(pred.rda$posterior[,1]))
-
 pred.tree.prob<-predict(tree, classif.test, "prob")
-roc.tree <- roc(classif.test$y, as.vector(pred.tree.prob[,1])) 
 pred.multi.logistreg.prob<-predict(multi.logistreg, classif.test, "prob")
-roc.logistreg <- roc(classif.test$y, as.vector(pred.multi.logistreg.prob[,1])) # OU roc.logistreg<-roc(classif.test$y,as.vector(pred.multi.logistreg.prob$posterior[,1])) 
-pred.nb.prob <- predict(fit.naive, newdata=classif.test, type="prob") # COMPRENDRE POURQUOI ON DOIT REFAIRE CETTE PREDICTION AVEC TYPE = PROB
-roc.nb <- roc(classif.test$y, as.vector(pred.nb.prob[,1]))
+pred.nb.prob <- predict(fit.naive, newdata=classif.test, type="prob") 
 
-# Area under the curve
-roc.lda        # 0.848
-roc.qda        # 0.6173
-roc.rda        # 0.8407
-roc.nb         # 0.916
-roc.tree       # 0.783
-roc.logistreg  # 0.81
-# NB > LDA > RDA > LOGISTIC REGRESSION > TREE > QDA
+#### Tentative multiroc
+library(pROC)
+# rf = randomForest(Species~., data = iris, ntree = 100)
+# predict(.., type = 'prob') returns a probability matrix
+# predictions <- as.numeric(predict(rf, iris, type = 'response'))
 
-plot(roc.nb, col='black') 
-plot(roc.lda, col='red',add=TRUE)
-plot(roc.qda, col='green', add=TRUE)
-plot(roc.rda, col='yellow', add=TRUE)
-plot(roc.tree, col='blue', add=TRUE)
-plot(roc.logistreg,col='purple',add=TRUE)
-legend("bottomright",legend=c("NB","LDA","QDA","RDA","TREE","Logistic Regression"),col=c("black","red","green","yellow","blue","purple"),lty=1:2)
+multiroc.lda <- multiclass.roc(classif.test$y,as.vector(pred.classif.lda$posterior[,1])) 
+multiroc.qda <- multiclass.roc(classif.test$y,as.vector(pred.qda$posterior[,1])) # OU roc.qda <- roc(classif.test$y,as.vector(pred.qda[,1]))
+multiroc.rda <- multiclass.roc(classif.test$y,as.vector(pred.rda$posterior[,1]))
+multiroc.tree <- multiclass.roc(classif.test$y, as.vector(pred.tree.prob[,1])) 
+multiroc.logistreg <- multiclass.roc(classif.test$y, as.vector(pred.multi.logistreg.prob[,1])) # OU roc.logistreg<-roc(classif.test$y,as.vector(pred.multi.logistreg.prob$posterior[,1])) 
+multiroc.nb <- multiclass.roc(classif.test$y, as.vector(pred.nb.prob[,1]))
+
+
+auc(multiroc.lda)
+auc(multiroc.qda)
+auc(multiroc.rda)
+auc(multiroc.tree)
+auc(multiroc.logistreg)
+auc(multiroc.nb)
+
+rs.lda <- multiroc.lda[['rocs']]
+rs.qda <- multiroc.qda[['rocs']]
+rs.rda <- multiroc.rda[['rocs']]
+rs.tree <- multiroc.tree[['rocs']]
+rs.logistreg <- multiroc.logistreg[['rocs']]
+rs.nb <- multiroc.nb[['rocs']]
+
+plot.roc(rs.lda[[1]], col='black')
+plot.roc(rs.qda[[1]], col='red', add=TRUE)
+plot.roc(rs.rda[[1]], col='green', add=TRUE)
+plot.roc(rs.tree[[1]], col='yellow', add=TRUE)
+plot.roc(rs.logistreg[[1]], col='blue', add=TRUE)
+plot.roc(rs.nb[[1]], col='purple', add=TRUE)
+legend("bottomright",legend=c("LDA","QDA","RDA","TREE","Logistic Regression", "NB"),col=c("black","red","green","yellow","blue","purple"),lty=1:2)
+
+# sapply(2:length(rs.lda),function(i) lines.roc(rs[[i]],col=i))
 
 
 ##### k-fold cross-validation
@@ -176,7 +189,7 @@ folds<-sample(1:K,n,replace=TRUE)
 cv.err<-matrix(0,ncol=1,nrow=K)
 
 plot.new()
-par(mfrow=c(2,3)) 
+par(mfrow=c(1,3)) 
 plot.cv.error<-function(data,x.title){
   #ic.error.bar<-function(x,lower,upper,length=0.1){
   #  arrows(x,upper,x,lower,angle=90,code=3,length=length,col='red')
@@ -218,23 +231,23 @@ for(k in 1:K){
 }
 plot.cv.error(cv.err,x.title = "RDA - 10 crossvalidation")
 
-for(k in 1:K){
-  fold.test<-classif[folds==k,]
-  fold.train<-classif[folds!=k,]
-  fold.classif<-multinom(y~.,data=fold.train)
-  fold.predict<-predict(fold.classif,newdata=fold.test)
-  cv.err[k,1]<-mean(fold.predict$class!=fold.test$y)
-}
-plot.cv.error(cv.err,x.title = "LOGISTIC REGRESSION - 10 crossvalidation")
+# for(k in 1:K){
+#   fold.test<-classif[folds==k,]
+#   fold.train<-classif[folds!=k,]
+#   fold.classif<-multinom(y~.,data=fold.train)
+#   fold.predict<-predict(fold.classif,newdata=fold.test)
+#   cv.err[k,1]<-mean(fold.predict$class!=fold.test$y)
+# }
+# plot.cv.error(cv.err,x.title = "LOGISTIC REGRESSION - 10 crossvalidation")
 
-for(k in 1:K){
-  fold.test<-classif[folds==k,]
-  fold.train<-classif[folds!=k,]
-  fold.classif<-naive_bayes(y~.,data=fold.train)
-  fold.predict<-predict(fold.classif,newdata=fold.test)
-  cv.err[k,1]<-mean(fold.predict$class!=fold.test$y)
-}
-plot.cv.error(cv.err,x.title = "Naive Bayes - 10 crossvalidation")
+# for(k in 1:K){
+#   fold.test<-classif[folds==k,]
+#   fold.train<-classif[folds!=k,]
+#   fold.classif<-naive_bayes(y~.,data=fold.train)
+#   fold.predict<-predict(fold.classif,newdata=fold.test)
+#   cv.err[k,1]<-mean(fold.predict$class!=fold.test$y)
+# }
+# plot.cv.error(cv.err,x.title = "Naive Bayes - 10 crossvalidation")
 
 
 
@@ -288,3 +301,4 @@ for(k in 1:30){
   plot(k,1-misClassError)
 }
 print(paste('KNN - maxAccuracy =', 1-err.knn))
+
